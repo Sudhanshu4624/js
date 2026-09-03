f7a95fe1b567f411e0f5576887b2861d49b6b8eb
Sudhanshu4624 <98212727+Sudhanshu4624@users.noreply.github.com>
2026-09-03 15:29:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// Documentations -> tc39 & mdn 
+// Keep the code readability very high 
+
+let name = "Sid"
+let age = 20
+let status;
+let salary = null
+let married = false
+// Data types
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof status);
+console.log(typeof salary);
+/*
+Commonly used datatypes
+number -> max upto 2^53
+bigint -> for bigger number
+string
+boolean -> true/false
+undefined
+null -> standalone value -> object dtype
+symbol -> unique
+*/
