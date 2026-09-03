7e4cc149657940e62b74fb05f56484224264010f
Sudhanshu4624 <98212727+Sudhanshu4624@users.noreply.github.com>
2026-09-03 14:47:52 +0000

    Variables and conatiner

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accId = 4626
+let accName = "Sid"
+var accPasscode = "1235"
+accCity = "Mumbai"
+let accState;
+
+// accId = 725 // Not allowed to change constant value
+
+console.log(accId);
+console.log(accName);
+accName = "Sudhanshu"
+accPasscode = "1236"
+accCity = "Banglore"
+
+console.table([accId, accName, accPasscode, accCity, accState])
