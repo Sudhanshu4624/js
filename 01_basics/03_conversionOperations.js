5108660f1ebae9dff7869ec542a3159401041b79
Sudhanshu4624 <98212727+Sudhanshu4624@users.noreply.github.com>
2026-09-03 15:51:36 +0000

    conversionDatatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let aNum = 46
+console.log(typeof aNum);
+
+let n2str = String(aNum)
+console.log(n2str);
+console.log(typeof n2str);
+
+/* 
+Similarly we can convert any datatype 
+using Number
+string "46" => 46  
+num+str "46abc" => NaN
+boolean true => 1; false => 0
+null -> 0
+undefined -> NaN
+
+Boolean
+1 => true; 0 => false
+"" => false
+"sid" => true
+
+*/
