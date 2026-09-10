67492b9d2b0e9f373a95911f650775ffbcbf7a57
Sudhanshu4624 <98212727+Sudhanshu4624@users.noreply.github.com>
2026-09-10 12:58:05 +0000

    execution

---
+++ 03_basics/05_executionFlow.js
@@ -0,0 +1,16 @@
+/*
+
+global execution phase --> this
+
+memory creation phase --> all definitions and values will be undefined for all var
+execution phase --> values will be assigned here to all var and also all the operations will be performed here
+
+function is called/run -> new executional context is created [new sandbox]
+[new variable environment + execution thread] 
+-->inside--> 
+memory phase, execution phase 
+the return value is returned to the global executional context
+----> after completion this executional context gets deleted
+
+call stack --> browser
+*/
