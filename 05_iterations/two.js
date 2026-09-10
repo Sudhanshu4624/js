7b6d50e3ad49fd321da94b76349c20a3359d039b
Sudhanshu4624 <98212727+Sudhanshu4624@users.noreply.github.com>
2026-09-10 18:46:39 +0000

    while - do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(i);
+//     i = i + 2
+// }
+
+// let newArray = ['mango','banana','kiwi','apple']
+
+// let a = 0
+// while (a < newArray.length) {
+//     console.log(newArray[a]);
+//     a++ // a = a+1
+// }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
